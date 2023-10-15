a15a5336ff0d9376d88abe73d9c02ed7afcca050
psychosymbol <psychosymbol@gmail.com>
2023-10-15 08:55:57 +0700

    add test camera control scene
    - make a camera control script to make the camera look around while player is moving a mouse around the viewport

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float maxAngleX = 10;
+    public float maxAngleY = 5;
+    private void Update()
+    {
+        Vector3 middleCenterBasedMouse =
+            (Camera.main.ScreenToViewportPoint(Input.mousePosition) //get mouse position base on cursor location on the game' viewport
+            + new Vector3(-0.5f, -0.5f, 0)) //set the value to make the center of the screen become zero position
+            * 2; //multiplyer to make the value we get round up to 1 ?
+
+        Debug.Log("try to base on middle screen as center : " + middleCenterBasedMouse);
+
+        gameObject.transform.eulerAngles = new Vector3(
+            (-middleCenterBasedMouse.y * maxAngleX),
+            (middleCenterBasedMouse.x * maxAngleY),
+            gameObject.transform.eulerAngles.z
+            );
+    }
+}
