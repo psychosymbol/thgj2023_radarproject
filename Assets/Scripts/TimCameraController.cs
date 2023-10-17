a281378253d55660a3989703169c5f47e2fc419f
korrakot-impact360 <korrakot.intanon@impact360design.com>
2023-10-17 07:59:48 +0700

    added free 3d assets from store

--- Assets/Scripts/TimCameraController.cs
+++ Assets/Scripts/TimCameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class TimCameraController : MonoBehaviour
 {
     public float maxAngleX = 10;
     public float maxAngleY = 5;
